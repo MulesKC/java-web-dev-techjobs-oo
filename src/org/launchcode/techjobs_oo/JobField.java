053f5dea99a5ae776f2bd01bb4e4caa3b430716f
MulesKC <rmmcp3@gmail.com>
2020-02-04 21:15:52 -0600

    finished all but bonus

--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -14,7 +14,11 @@ public abstract class JobField {
 
     public JobField(String value) {
         this();
-        this.value = value;
+        if (value == "") {
+            this.value = "Data not available";
+        } else {
+            this.value = value;
+        }
     }
 
     @Override
--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -7,5 +7,5 @@ public class PositionType extends JobField {
     public PositionType(String value) {
         super(value);
     }
-    
+
 }
